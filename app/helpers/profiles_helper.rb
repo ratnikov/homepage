78ac0bdbc3e636fe1e7a944a1da051611130f225
Dmitry Ratnikov <ratnikov@gmail.com>
2010-11-20 19:19:25 -0500

    Styled the profile page.

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,5 +1,6 @@
 module ProfilesHelper
-  def gravatar_tag(email)
-    image_tag ::Gravatar.url(email), :alt => 'Gravatar image'
+  def gravatar_tag(email, options = nil)
+    size = options && options[:size]
+    image_tag ::Gravatar.url(email, :size => size), :alt => 'Gravatar image'
   end
 end
--- lib/gravatar.rb
+++ lib/gravatar.rb
@@ -3,7 +3,8 @@ require 'digest/md5'
 module Gravatar
   extend self
   
-  def url(email)
-    "http://www.gravatar.com/avatar/%s" % [ Digest::MD5.hexdigest(email.downcase) ]
+  def url(email, options = nil)
+    size = options && options[:size] || 80
+    "http://www.gravatar.com/avatar/%s?size=%s" % [ Digest::MD5.hexdigest(email.downcase), size ]
   end
 end
